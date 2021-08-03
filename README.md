3695ca5af30d32225dc459cf49e5710e5cda3bee
Danil Melentev <mejleht@yandex.ru>
2021-08-03 15:36:03 +0300

    refactor: finally

--- README.md
+++ README.md
@@ -1,6 +1,41 @@
-# architect-portfolio.github.io
-Architect Portfolio
-JQuery
-Slick Slider
+# Architect Portfolio
 
-### https://melentq.github.io/architect-portfolio/
+## About
+
+Отзывчивый лендинг, портфолио архитектора.
+Коммерческий проект
+https://oleg-proekt.ru
+
+## Tech Stack
+
+* HTML
+* CSS - SCSS
+* JS - ES6
+* Webpack
+
+* [BEM](https://ru.bem.info/)
+* responsive design
+* accessibility (semantic html)
+* cross-browser
+* images are optimized with [TinyPNG](https://tinypng.com/), encoded in .webp
+* SVG are optimized with [SVGOMG](https://jakearchibald.github.io/svgomg/)
+
+## Libs
+
+* [Normalize.css](https://necolas.github.io/normalize.css/)
+* [Swiper](https://swiperjs.com/) - for slider
+
+## Get Started
+
+### Developer mode
+
+`npm run dev`
+
+### Build mode
+
+`npm run build`
+
+## Links
+
+[GH Pages](https://melentq.github.io/architect-portfolio)
+[oleg-proekt.ru](https://oleg-proekt.ru)
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,9 @@ import '../pages/index.scss';
 import '../utils/smoothScroll';
 import {gallerySliderSelector, gallerySliderSettings} from '../utils/constants';
 
+import ImageModalPanel from '../components/ImageModalPanel';
+import ModalPanel from '../components/ModalPanel.js';
+
 const gallerySlider = new Swiper(gallerySliderSelector, gallerySliderSettings);
 
 const header = document.querySelector('.header');
@@ -22,3 +25,43 @@ document.addEventListener('scroll', () => {
   }
   scrollPrev = scrolled;
 }, {passive: true})
+
+const setIsModalOpen = (isOpen) => {
+  isModalOpen = isOpen;
+}
+const onOpenModal = () => {
+  header.classList.add('header_hidden');
+  setIsModalOpen(true);
+}
+const onCloseModal = () => {
+  setIsModalOpen(false);
+}
+
+// Modal panel for menu
+const menuModal = new ModalPanel({
+  selector: '#menuModal',
+  onOpen: onOpenModal,
+  onClose: onCloseModal
+})
+menuModal.setEventListeners();
+const openMenuBtn = document.querySelector('.header__menu-btn');
+openMenuBtn.addEventListener('click', menuModal.openModalPanel.bind(menuModal));
+const menuLinks = document.querySelectorAll('.menu__link');
+menuLinks.forEach(link => link.addEventListener('click', menuModal.closeModalPanel.bind(menuModal)))
+
+// Modal panel for gallery
+const cardModal = new ImageModalPanel({
+  selector: '#cardModal',
+  onOpen: onOpenModal,
+  onClose: onCloseModal
+})
+cardModal.setEventListeners();
+
+const galleryCards = document.querySelectorAll('.slide');
+galleryCards.forEach(card => {
+  const cardPath = card.querySelector('.slide__image').src;
+  const cardDescription = card.querySelector('.slide__description').textContent;
+  card.addEventListener('click', () => {
+    cardModal.openModalPanel(cardPath, cardDescription)
+  })
+})
